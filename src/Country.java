91d4a7354e0e747ef72de720f83aa5f7116a78e4
Karl Clifford <karl@khsclifford.com>
2021-12-03 23:51:58 +0000

    Commented out test data.

--- src/Country.java
+++ src/Country.java
@@ -41,8 +41,8 @@ public class Country {
         //Calculate the population of all cities
         int totalPopulationInAllCities =
                 cities.values().stream().mapToInt(City::getPopulation).sum();
-        System.out.println("HEY " + totalPopulationInAllCities + '\n' + this.population);
-        System.out.println(this.population - totalPopulationInAllCities);
+        //System.out.println("HEY " + totalPopulationInAllCities + '\n' + this.population);
+        //System.out.println(this.population - totalPopulationInAllCities);
 
         //if (this.population == 0) {
             //totalPopulationInAllCities *= +-1;
